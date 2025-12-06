d98f3f6ee52d3725a43c08c3086dca84725d5487
Alex <lisatthu35@gmail.com>
2025-12-06 20:37:32 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -80,7 +80,7 @@ $maindomain = $_SERVER['HTTP_HOST'];
 $protocol = "https";
 $url = $_SERVER["REQUEST_URI"];
 
-$url = str_replace("api/index.php/", "", $url);
+$url = str_replace("/api/index.php/", "", $url);
 var_dump($url); 
 $parts = parse_url($url);
 var_dump($parts); die();
