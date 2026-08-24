5be837aa618bf38aebce698c2aa6fda2b65888bb
Alex <lisatthu35@gmail.com>
2026-08-24 13:26:43 +0700

    Add JSON error response for empty maindomain
    Return JSON error response when maindomain is empty.

--- api/index.php
+++ api/index.php
@@ -116,6 +116,11 @@ if (empty($url)) {
 }
 if (empty($maindomain)) {
     $maindomain = parse_url($url, PHP_URL_HOST);
+    echo json_encode([
+        'error' => true,
+        'message' => $maindomain
+    ]);
+    exit;
 }
 
 // Validate URL cơ bản
